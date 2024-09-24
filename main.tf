260eb704d72a4f05d0eba95bb8063c9a23e4690f
Tzukflake <andreizh94@gmail.com>
2024-09-24 23:31:38 +0200

    initial code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "tt-bucket"
-  project       = "tt-dev-001"
-  location      = "eu-north1"
-  force_destroy = true
+  name                     = "tt-bucket"
+  project                  = "tt-dev-001"
+  location                 = "eu-north1"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
