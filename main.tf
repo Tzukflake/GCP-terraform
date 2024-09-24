1d9582f02c4af3f7bf972b70916d9f8b31791ca4
Tzukflake <andreizh94@gmail.com>
2024-09-24 22:37:56 +0200

    initial code

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+provider "google" {
+  credentials = file("C:/Users/Andreizh/Downloads/git-gcp-terraform-8ca39467befd.json")  # Use forward slashes in the file path
+  project     = "git-gcp-terraform"
+  region      = "europe-north1"  # Use region only (without the zone suffix '-a')
+}
+
 resource "google_storage_bucket" "my-bucket" {
   name          = "tt-bucket"
-  location      = "US"
+  location      = "EU"
   force_destroy = true
 
   public_access_prevention = "enforced"
