96c78948cb18a67fb965930fff70638d9d5937bc
Tzukflake <andreizh94@gmail.com>
2024-09-24 23:31:03 +0200

    initial code

--- main.tf
+++ main.tf
@@ -1,25 +1,7 @@
-provider "google" {
-  credentials = file(var.credentials_file)
-  project     = var.project_id
-  region      = var.region
-}
-
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-bucket"
-  location                 = "EU"
-  force_destroy            = true
+  name          = "tt-bucket"
+  project       = "tt-dev-001"
+  location      = "eu-north1"
+  force_destroy = true
   public_access_prevention = "enforced"
-}
-
-
-variable "credentials_file" {
-  description = "Path to the GCP credentials file"
-}
-
-variable "project_id" {
-  description = "GCP project ID"
-}
-
-variable "region" {
-  description = "GCP region"
 }
