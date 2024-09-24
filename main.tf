870c0330bc59ba1646c5930b7b7383bd04d242ee
Tzukflake <andreizh94@gmail.com>
2024-09-24 23:40:22 +0200

    grant service role

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "tt-bucket"
   project                  = "tt-dev-001"
-  location                 = "eu"
+  location                 = "europe-north1"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
